f046882cf7a9f463516af0759cad5ec48e7e27a2
nick <nick.zwart@gmail.com>
2015-11-02 12:09:09 -0700

    Added arg parser and cleaned up plist generator code.

--- mkapp.py
+++ mkapp.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python
+# Author: Nick Zwart
+# Date: 2015oct31 
 # Adapted from SO: http://stackoverflow.com/questions/7404792/how-to-create-mac-application-bundle-for-python-script-via-python
 
-import stat
+import os
 import sys
-import os, os.path
+import stat
+import shutil
+import optparse
 
-assert len(sys.argv) > 1
-apppath = sys.argv[1]
-assert os.path.splitext(apppath)[1] == ".app"
+parser = optparse.OptionParser()
+parser.add_option("-n", "--name", dest='name', help="The name of this app.")
+parser.add_option("-v", "--version", dest='version', help="The version of this app.", default='0.1')
+parser.add_option("-i", "--icon", dest='icon_file', help="Icon file to be used in the bundle.")
+options, args = parser.parse_args()
+
+if options.icon_file is not None:
+    if not os.path.isfile(options.icon_file):
+        print "The supplied icon path doesn\'t point to a valid file."
+        parser.print_help()
+        sys.exit(1)
+    if os.path.splitext(options.icon_file)[1] != ".icns":
+        print "The supplied icon path must point to an .icns file."
+        parser.print_help()
+        sys.exit(1)
+if options.name == '' or options.name is None:
+    print "The app name is a required input."
+    parser.print_help()
+    sys.exit(1)
+
+CFBundleVersion = options.version
+CFBundleName = options.name
+CFBundleGetInfoString = CFBundleName + " " + CFBundleVersion
+CFBundleShortVersionString = CFBundleGetInfoString
+CFBundleIdentifier = "com.gpilab."+CFBundleName
+CFBundleIconFile = 'app.icns'
+
+apppath = './'+CFBundleName+'.app'
 
 os.mkdir(apppath)
 os.mkdir(apppath + "/Contents")
 os.mkdir(apppath + "/Contents/MacOS")
+os.mkdir(apppath + "/Contents/Resources")
 
-CFBundleVersion = "1.0.0"
-CFBundleName = "Test"
-CFBundleGetInfoString = CFBundleName + " " + CFBundleVersion
-CFBundleShortVersionString = CFBundleGetInfoString
-CFBundleIdentifier = "com.gpilab.test"
+if options.icon_file is not None:
+    shutil.copy(options.icon_file, apppath + "/Contents/Resources/" + CFBundleIconFile)
 
 info_plist = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -30,7 +57,7 @@ info_plist = """<?xml version="1.0" encoding="UTF-8"?>
     <key>CFBundleGetInfoString</key>
     <string>%s</string>
     <key>CFBundleIconFile</key>
-    <string>app.icns</string>
+    <string>%s</string>
     <key>CFBundleIdentifier</key>
     <string>%s</string>
     <key>CFBundleInfoDictionaryVersion</key>
@@ -76,7 +103,7 @@ main()
 """
 
 with open(apppath + "/Contents/Info.plist", "w") as f:
-    f.write(info_plist % (CFBundleGetInfoString, CFBundleIdentifier, CFBundleName, CFBundleShortVersionString, CFBundleVersion))
+    f.write(info_plist % (CFBundleGetInfoString, CFBundleIconFile, CFBundleIdentifier, CFBundleName, CFBundleShortVersionString, CFBundleVersion))
 
 with open(apppath + "/Contents/PkgInfo", "w") as f:
     f.write("APPL????")

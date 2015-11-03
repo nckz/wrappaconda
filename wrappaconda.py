3744e337f4906ecb58ffa43fa5be05ea2d3bd465
nick <nick.zwart@gmail.com>
2015-11-03 15:20:27 -0700

    python 2 and 3 compatible.

--- wrappaconda.py
+++ wrappaconda.py
@@ -3,6 +3,8 @@
 # Date: 2015oct31 
 # Adapted from SO: http://stackoverflow.com/questions/7404792/how-to-create-mac-application-bundle-for-python-script-via-python
 
+from __future__ import print_function
+
 import os
 import sys
 import stat
@@ -20,7 +22,7 @@ except:
         subprocess.check_output('command -v curl >/dev/null 2>&1;', shell=True)
         get_cmd = 'curl -O -C - '
     except:
-        print "This script requires \'wget\' or \'curl\' and neither were found."
+        print("This script requires \'wget\' or \'curl\' and neither were found.")
         raise
 
 # get user input
@@ -31,30 +33,30 @@ parser.add_option("-i", "--icon", dest='icon_file', help="Icon file to be used i
 parser.add_option("-t", "--target", dest='target', help="The python script to be executed by the app.")
 parser.add_option("-c", "--channel", dest='channel', help="The Anaconda.org package channel.", default='defaults')
 parser.add_option("-p", "--package", dest='package', help="The package name.")
-parser.add_option("--py2", action="store_true", dest='py2', help="Icon file to be used in the bundle.")
+parser.add_option("--py2", action="store_true", dest='py2', help="Choose the distro python version (defaults to py3).")
 parser.add_option("-o", "--over-write", action="store_true", dest='overwrite', help="Overwrite existing apps. Use caution!!!")
 options, args = parser.parse_args()
 
 # check for input errors
 if options.icon_file is not None:
     if not os.path.isfile(options.icon_file):
-        print "The supplied icon path doesn\'t point to a valid file."
+        print("The supplied icon path doesn\'t point to a valid file.")
         parser.print_help()
         sys.exit(1)
     if os.path.splitext(options.icon_file)[1] != ".icns":
-        print "The supplied icon path must point to an .icns file."
+        print("The supplied icon path must point to an .icns file.")
         parser.print_help()
         sys.exit(1)
 if options.name == '' or options.name is None:
-    print "The app name is a required input."
+    print("The app name is a required input.")
     parser.print_help()
     sys.exit(1)
 if options.target == '' or options.target is None:
-    print "The app target is a required input."
+    print("The app target is a required input.")
     parser.print_help()
     sys.exit(1)
 if options.package == '' or options.package is None:
-    print "An app package is a required input."
+    print("An app package is a required input.")
     parser.print_help()
     sys.exit(1)
 
@@ -82,15 +84,15 @@ conda_path = apppath + "/Contents/Resources/miniconda/bin/conda"
 
 if os.path.exists(apppath):
     if options.overwrite:
-        print "Removing existing path: "+apppath
+        print("Removing existing path: "+apppath)
         try:
             with open(id_file_path, 'r') as f:
                 assert f.read().count('Wr[App]-A-Conda') > 0
             shutil.rmtree(apppath)
         except:
-            print "The app \'"+apppath+"\' cannot be verified for deletion, exiting..."
+            print("The app \'"+apppath+"\' cannot be verified for deletion, exiting...")
     else:
-        print "The app \'"+apppath+"\' already exists, exiting..."
+        print("The app \'"+apppath+"\' already exists, exiting...")
 
 # build the .app directory and supporting files
 try:
@@ -100,7 +102,7 @@ try:
     os.mkdir(apppath + "/Contents/Resources")
 except OSError as e:
     if (e[0] == errno.EPERM):
-        print "You must have root permissions to write to /Applications."
+        print("You must have root permissions to write to /Applications.")
     sys.exit(1)
 
 if options.icon_file is not None:
@@ -163,21 +165,23 @@ os.chmod(apppath + "/Contents/MacOS/main.py", oldmode | stat.S_IXUSR | stat.S_IX
 try:
     subprocess.check_output(get_cmd+MINICONDA_WEB+MINICONDA_OSX, shell=True)
 except:
-    print "Failed to download miniconda."
+    print("Failed to download miniconda.")
 
 # install miniconda
 try:
     os.chmod('./'+MINICONDA_OSX, 0o777)
     subprocess.check_output('./'+MINICONDA_OSX+' -b -p '+apppath+"/Contents/Resources/miniconda", shell=True)
 except:
-    print "Failed to run miniconda."
+    print("Failed to run miniconda.")
 
 # install central conda package
 try:
-    subprocess.check_output(conda_path+' install -y -c '+options.channel+' '+options.package, shell=True)
-    subprocess.check_output(conda_path+' -t -i -p -l -y', shell=True)
+    conda_cmd = conda_path+' install -y -c '+options.channel+' '+options.package
+    print(conda_cmd)
+    subprocess.check_output(conda_cmd, shell=True)
+    subprocess.check_output(conda_path+' clean -t -i -p -l -y', shell=True)
 except:
-    print "Failed to run conda."
+    print("Failed to run conda.")
     raise
 
-print apppath + " has been created."
+print(apppath + " has been created.")
